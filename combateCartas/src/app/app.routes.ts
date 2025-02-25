0fe77578bf8d95098850b600510899242670780d
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-02-25 13:07:47 +0100

    partida

--- combateCartas/src/app/app.routes.ts
+++ combateCartas/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import {PartidaComponent} from "./partida/partida.component";
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'partida',
+    component: PartidaComponent,
   },
   {
     path: '',
--- combateCartas/src/app/partida/partida.component.ts
+++ combateCartas/src/app/partida/partida.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {IonBackButton, IonButton, IonCol, IonContent, IonGrid, IonRow} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-partida',
   templateUrl: './partida.component.html',
   styleUrls: ['./partida.component.scss'],
+  imports: [
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonBackButton,
+    IonButton
+  ]
 })
 export class PartidaComponent  implements OnInit {
 
