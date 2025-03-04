c923f2b92827206e4dd798b057436d503570d315
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-03-04 14:19:30 +0100

    bucle jugable

--- combateCartas/src/app/partida/partida.component.ts
+++ combateCartas/src/app/partida/partida.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, numberAttribute, OnInit} from '@angular/core';
 import {IonBackButton, IonButton, IonCol, IonContent, IonGrid, IonProgressBar, IonRow} from "@ionic/angular/standalone";
 
 @Component({
@@ -19,10 +19,11 @@ export class PartidaComponent  implements OnInit
   constructor() { }
   ngOnInit()
   {
-    this.iniciarRelog();
+    this.iniciarRonda();
   }
 
   // ****** Declaraciones ***** //
+  _numPregunta_i: number = 0;
   _tiempo_i: number = 30;
   _vida_i: number = 100;
 
@@ -33,13 +34,56 @@ export class PartidaComponent  implements OnInit
   _respuesta3_s: string = "Respuesta 3";
   _respuesta4_s: string = "Respuesta 4";
 
+  // variables de intervalo
   _intervaloID: any;
 
 
   // ****** Funciones ***** //
+  iniciarRonda()
+  {
+    this._numPregunta_i = this.generarRandom(0, this._preguntas.length);
+    console.log("Num pregunta: " + this._numPregunta_i);
+
+    this._pregunta_s = this._preguntas[this._numPregunta_i].pregunta;
+    this._respuesta1_s = this._preguntas[this._numPregunta_i].respuestas[0].respuesta;
+    this._respuesta2_s = this._preguntas[this._numPregunta_i].respuestas[1].respuesta;
+    this._respuesta2_s = this._preguntas[this._numPregunta_i].respuestas[2].respuesta;
+    this._respuesta2_s = this._preguntas[this._numPregunta_i].respuestas[3].respuesta;
+
+    this.iniciarRelog();
+  }
+
+  generarRandom(_min_i:number, _max_i:number): number
+  {
+    return Math.floor
+    (
+      Math.random() * (_max_i - _min_i)
+    ) + _min_i;
+  }
+
   responderPregunta(_respuesta_i: number)
   {
-    console.log("Respuesta: " + _respuesta_i);
+    clearInterval(this._intervaloID);
+
+    if (_respuesta_i == -1)
+    {
+      this._vida_i -= 10;
+    }
+    else if (!this._preguntas[this._numPregunta_i].respuestas[_respuesta_i].correcta)
+    {
+      this._vida_i -= 20;
+    }
+    else
+    {
+      console.log("Respuesta correcta");
+      this.iniciarRonda();
+    }
+
+    if (this._vida_i <= 0)
+    {
+      // TODO: Perder partida.
+      console.log("partida perdida");
+    }
   }
 
   iniciarRelog()
@@ -49,12 +93,13 @@ export class PartidaComponent  implements OnInit
       if (this._tiempo_i > 0)
         this._tiempo_i--;
       else
-        clearInterval(this._intervaloID);
+        this.responderPregunta(-1);
+
     }, 1000);
   }
 
   //
-  preguntas: any[] =
+  _preguntas: any[] =
   [
     {
       pregunta: "Pregunta 1",
@@ -68,6 +113,26 @@ export class PartidaComponent  implements OnInit
     },
     {
       pregunta: "Pregunta 2",
+      respuestas:
+      [
+        {respuesta: "Respuesta 1", correcta: true},
+        {respuesta: "Respuesta 2", correcta: false},
+        {respuesta: "Respuesta 3", correcta: false},
+        {respuesta: "Respuesta 4", correcta: false}
+      ]
+    },
+    {
+      pregunta: "Pregunta 3",
+      respuestas:
+      [
+        {respuesta: "Respuesta 1", correcta: true},
+        {respuesta: "Respuesta 2", correcta: false},
+        {respuesta: "Respuesta 3", correcta: false},
+        {respuesta: "Respuesta 4", correcta: false}
+      ]
+    },
+    {
+      pregunta: "Pregunta 4",
       respuestas:
       [
         {respuesta: "Respuesta 1", correcta: true},
