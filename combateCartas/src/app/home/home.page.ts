7079be194f6b37342ea0e29a3ae25d90a135c557
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-03-10 22:13:08 +0100

    AppModule

--- combateCartas/src/app/home/home.page.ts
+++ combateCartas/src/app/home/home.page.ts
@@ -1,21 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
   IonContent,
   IonGrid,
   IonCol,
   IonRow,
-  IonButton, IonImg
+  IonButton,
+  IonImg
 } from '@ionic/angular/standalone';
+import {DTOlistarJugadores} from "../zzz_dtos/listar_jugadores/DTOlistarJugadores";
+import {SerJugadorService} from "../zzz_servicios/ser-jugador.service";
+import {UsuarioComponent} from "../usuario/usuario.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonButton, IonImg],
+  imports: [IonContent, IonGrid, IonCol, IonRow, IonButton, IonImg, UsuarioComponent, NgForOf],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit
+{
+  constructor(private _SerJugador: SerJugadorService) {}
+  ngOnInit()
+  {
+    this._SerJugador.listarJugadores().subscribe
+    (
+      (data: DTOlistarJugadores) =>
+      {
+        this._jugadores = data;
+      },
+      error =>
+      {
+        console.error('Web:(Home:ngOnInit):', error);
+      }
+    )
+  }
+
+  // --- Variables --- //
+  _jugadores: DTOlistarJugadores | undefined;
 }
